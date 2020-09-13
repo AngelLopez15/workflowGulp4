0e9e06b17cdb3aa8d70bdd4e018b0b891d9cb9a6
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-09-13 03:04:26 -0500

    Configuracion finalizada

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,35 @@ const htmlmin = require('gulp-htmlmin')
 const GulpPostCss = require('gulp-postcss')
 const cssnano = require('cssnano')
 const autoprefixer = require('autoprefixer')
+
 // PUG
 const GulpPug = require('gulp-pug')
+
 // SASS
 const sass = require('gulp-sass')
-const { src } = require('gulp')
+// const { src } = require('gulp')
+
 // clean css
 const purgecss = require('gulp-purgecss')
+
+// limpiar cache del navegador
+const cachebust = require('gulp-cache-bust');
+
+// optimizar imagenes para web con imagen min
+const imagemin = require('gulp-imagemin');
+
+// Browser-sync
+// init es para levantar un servidor de desarrollo
+// stream inyecta el css al hacer cambios
+// reload recarga la pagina al hacer 
+const browserSync = require('browser-sync').create();
+const reload      = browserSync.reload;
+
+// plumber
+// ponerlo en todas las tareas despues del src
+const gulpPlumber = require('gulp-plumber')
+
+
 
 // constante para pug
 const production = false
@@ -29,6 +51,7 @@ const cssPackge = [
 gulp.task('html-min', ()=>{
   return gulp
     .src('./src/*.html')
+    .pipe(gulpPlumber())
     .pipe(htmlmin({
       collapseWhitespace: true,
       removeComments: true
@@ -39,14 +62,17 @@ gulp.task('html-min', ()=>{
 gulp.task('styles', ()=>{
   return gulp
     .src('./src/css/*.css')
+    .pipe(gulpPlumber())
     .pipe(concat('styles-min.css'))
     .pipe(GulpPostCss(cssPackge))
     .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream())
 })
 
 gulp.task('babel', ()=>{
   return gulp
     .src('./src/js/*.js')
+    .pipe(gulpPlumber())
     .pipe(concat('scripts-min.js'))
     .pipe(babel())
     .pipe(terser())
@@ -56,8 +82,12 @@ gulp.task('babel', ()=>{
 // PUG
 gulp.task('views',()=>{
   return gulp.src('./src/views/pages/*.pug')
+    .pipe(gulpPlumber())
     .pipe(GulpPug({
       pretty: production ? false : true 
+    }))
+    .pipe(cachebust({
+      type:'timestamp'
     }))
     .pipe(gulp.dest('public'))
 })
@@ -65,23 +95,45 @@ gulp.task('views',()=>{
 // SASS
 gulp.task('sass',()=>{
   return gulp.src('./src/scss/styles.scss')
+    .pipe(gulpPlumber())
     .pipe(sass({
       outputStyle: 'compressed'
     }))
     .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream())
 })
 
 // limpiar css 
 gulp.task('clean', ()=>{
   return gulp.src('./public/css/styles.css')
-    .pipe()
+    .pipe(gulpPlumber())
+    .pipe(purgecss({
+      content:['./public/*.html']
+    }))
+    .pipe(gulp.dest('./public/css'))
 })
+
+// comprimir imagenes
+gulp.task('imgmin',()=>{
+  return gulp.src('./src/images/*')
+    .pipe(gulpPlumber())
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced:true}),
+      imagemin.mozjpeg({quality:30, progressive:true}),
+      imagemin.optipng({optimizationLevel:1})
+    ]))
+    .pipe(gulp.dest('./public/images'))
+})
+
 
 // haciendo el watch
 gulp.task('default', ()=>{
-  gulp.watch('./src/*.html', gulp.series('html-min'))
-  gulp.watch('./src/css/*.css', gulp.series('styles'))
-  gulp.watch('./src/views/**/*.pug', gulp.series('views'))
-  gulp.watch('./src/scss/**/*.pug', gulp.series('scss'))
-  gulp.watch('./src/js/*.js', gulp.series('babel'))
+  browserSync.init({
+    server:'./public'
+  })
+  // gulp.watch('./src/*.html', gulp.series('html-min')).on("change", reload)
+  // gulp.watch('./src/css/*.css', gulp.series('styles'))
+  gulp.watch('./src/views/**/*.pug', gulp.series('views')).on("change", reload)
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'))
+  gulp.watch('./src/js/*.js', gulp.series('babel')).on("change", reload)
 })
